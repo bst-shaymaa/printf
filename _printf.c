6ddbfe54a231bbe514b9018fadf63f7c9a8b47d0
Chaymae Boussetta <boussettachaymae@gmail.com>
2023-11-14 13:07:24 +0100

    debugging

--- _printf.c
+++ _printf.c
@@ -22,6 +22,8 @@ int _printf(const char * const format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+
+Here:
 	while (format[i] != '\0')
 	{
 		j = 13;
@@ -42,3 +44,4 @@ int _printf(const char * const format, ...)
 	va_end(args);
 	return (length);
 }
+
